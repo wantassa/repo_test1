037660c4c3c5555bfa30e956a0471d4098a13e93
wantassa <bolangjake@gmail.com>
2021-02-17 11:17:29 +0700

    menambahkan index.php

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<!--Baris Komentar-->
+	<head>
+		<title>BELAJAR Dasar HTML </title>
+	</head>
+
+	<body bgcolor ="yellow">
+		<h1 align = "center"><font color="greend"> HELLO WORLD </font/h1>
+		<h2 align = "center"> *****BIODATAKU***** </h2>
+
+                <hr>
+		<font color = "blue">
+		<body>
+		<table border=1>
+		<tr>
+		<th> No </th>
+		 <th>Nama</th> 
+		 <th>Kelas</th>
+		 <th>Alamat </th>
+		 <th>Tempat/tgl</th>
+		 <th>Asal sekolah </th>		
+		</tr>
+		
+
+		<tr>
+		<td> 1  </td>
+		<td> M.IKHSAN WANTASSA </td>
+		<td>Xl RPL1</TD>
+		<td>Griya Panorama Permai</td>
+		<td>Pekanbaru/05,09,2003</td>		
+		<td>SMK7</td>
+		</tr>
+		</font></table>
+		<hr/>
+		
+ 
+        </body>
+
+
+
+		
+		
+
+
+	</body>
+
+</html> 
